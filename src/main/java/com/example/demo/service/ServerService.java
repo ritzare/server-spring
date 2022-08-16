6baae7306ccc5aba2b6acbe5fce315bdf190a66d
ritzare <joaovictor-fza@hotmail.com>
2022-08-15 22:59:52 -0300

    add method create and ping

--- src/main/java/com/example/demo/service/ServerService.java
+++ src/main/java/com/example/demo/service/ServerService.java
@@ -1,11 +1,12 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Server;
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> List(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/com/example/demo/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/ServerServiceImpl.java
@@ -1,19 +1,39 @@
 package com.example.demo.service.implementation;
 
 import com.example.demo.domain.Server;
+import com.example.demo.enumeration.Status;
+import com.example.demo.repo.ServerRepo;
 import com.example.demo.service.ServerService;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
-
+    private final ServerRepo serverRepo;
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImagerUrl());
+        return serverRepo.save(server);
     }
 
+
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
@@ -33,6 +53,11 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+
+    private String setServerImagerUrl() {
         return null;
     }
 }
