00db86278849570cfe3673e6e467c5486009e87b
ritzare <joaovictor-fza@hotmail.com>
2022-08-20 20:18:14 -0300

    add new endpoints

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+import static com.example.demo.enumeration.Status.SERVER_DOWN;
+import static com.example.demo.enumeration.Status.SERVER_UP;
+
+import com.example.demo.domain.Server;
+import com.example.demo.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +16,16 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+          serverRepo.save(new Server(null, "192.168.1.169", "Ubuntu Linux", "16 GB", "Personal PC",
+                  "http://localhost:8080/server/image/server1.png", SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower",
+                    "http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+            serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server",
+                    "http://localhost:8080/server/image/server3.png", SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB",
+                    "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
+        };
+    }
 }
--- src/main/java/com/example/demo/resource/ServerResource.java
+++ src/main/java/com/example/demo/resource/ServerResource.java
@@ -5,15 +5,19 @@ import static java.time.LocalDateTime.now;
 import static java.util.Map.of;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 import com.example.demo.domain.Response;
 import com.example.demo.domain.Server;
 import com.example.demo.enumeration.Status;
 import com.example.demo.service.implementation.ServerServiceImpl;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,7 +46,7 @@ public class ServerResource {
         );
     }
 
-    @GetMapping("/ping/{idAddress}")
+    @GetMapping("/ping/{ipAddress}")
     public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress)
     throws IOException {
         Server server = serverService.ping(ipAddress);
@@ -71,6 +75,38 @@ public class ServerResource {
         );
     }
 
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("server", serverService.get(id) ))
+                        .message("Server Retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("deleted", serverService.delete(id) ))
+                        .message("Server deleted")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+
+    @GetMapping(path = "/image/{fileName}" , produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Documents/estudo/images/" + fileName));
+
+    }
 }
 
 
